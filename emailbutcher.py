7a6dc27324059a07e78e607c0866c5ffbc5a6ebd
rcmeou <rmcnannay@eou.edu>
2024-11-19 20:04:17 -0800

    Update emailbutcher.py
    Added a search of the IP and Domain with Whois. I removed the Shodan API that I was planning to use as I want this to remain simple to use for now.

--- emailbutcher.py
+++ emailbutcher.py
@@ -1,12 +1,13 @@
 import re                        # regular expression library to search for the IP and domain
 import requests                  # requests library to make HTTP requests
 import whois                     # whois library to get information about the domain
+from ipwhois import IPWhois      # obtain ISP from the IP address
 import dns.resolver              # dns.resolver library to get information about the domain
 import socket                    # socket library to get information about the domain
-from email import parser   # email.parser library to get information about the domain
+from email import parser    # email.parser library to get information about the domain
 import sys                       # sys library to open file as command line argument
 
-SHODAN_API = "PUT API KEY HERE"
+
 
 
 #check if email file is imported using command line argument
@@ -40,9 +41,48 @@ for header, value in email_message.items():
 
     domains = re.findall(domain_search, str(value))
     domains_set.update(domains)
-
+print("\n====== IP Addresses ======")
 for ip_addr in ip_addresses_set:
-    print("IP Address: ", ip_addr)
+    print("  ", ip_addr)
 
+#remove @ symbol in the domain set
+domains_set = {domain.replace('@', '') for domain in domains_set}
+
+print("\n====== Domains ======")
 for domain in domains_set:
-    print("Domain: ", domain)
+    print("  ", domain)
+
+# use the IP's from the ip_addresses_set to search whois
+print("\n====== Whois Domain Lookup ======")
+for domain in domains_set:
+    whois_info = whois.whois(domain)
+    print("  Domain:", domain)
+    print("    Registrar:", whois_info.registrar)
+    print("    Creation Date:", whois_info.creation_date)
+    print("    Expiration Date:", whois_info.expiration_date)
+    print("    Name Servers:", whois_info.name_servers)
+    print("    Status:", whois_info.status)
+    print("    Email:", whois_info.emails)
+    print("    Organization:", whois_info.org)
+    print("    Address:", whois_info.address)
+    print("    City:", whois_info.city)
+    print("    State:", whois_info.state)
+    print("    Zipcode:", whois_info.zipcode)
+    print("    Country:", whois_info.country) in ip_addresses_set
+
+print("\n====== Whois IP Lookup ======")
+for ip_addr in ip_addresses_set:
+    whois_info = whois.whois(ip_addr)
+    print("  IP:", ip_addr)
+    print("    Registrar:", whois_info.registrar)
+    print("    Creation Date:", whois_info.creation_date)
+    print("    Expiration Date:", whois_info.expiration_date)
+    print("    Name Servers:", whois_info.name_servers)
+    print("    Status:", whois_info.status)
+    print("    Email:", whois_info.emails)
+    print("    Organization:", whois_info.org)
+    print("    Address:", whois_info.address)
+    print("    City:", whois_info.city)
+    print("    State:", whois_info.state)
+    print("    Zipcode:", whois_info.zipcode)
+    print("    Country:", whois_info.country)
